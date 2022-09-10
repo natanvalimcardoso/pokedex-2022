5028b5f9f5201518cc58450844257c2c78100620
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-09-10 15:19:24 -0300

    imp clean arquiteture

---
+++ lib/app/modules/pokedex/domain/usecases/get_all_pokemon_usecase.dart
@@ -0,0 +1,12 @@
+// As usecases são responsáveis por fazer a ponte entre a camada de domínio e a camada de infraestrutura (data)
+// Ela é a implementação da regra de negócio
+
+abstract class IGetAllPokemonUsecase {
+  // Future<List<FailurePokedex, List<Pokemon>>> call(); EXEMPLO, COLCOAR either(dartz)
+
+}
+
+class GetAllPokemonUsecase implements IGetAllPokemonUsecase {
+  //@OVERRIDE
+  // Future<List<FailurePokedex, List<Pokemon>>> call(); EXEMPLO, COLCOAR either(dartz)
+}
