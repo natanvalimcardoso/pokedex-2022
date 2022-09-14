18228b9918295384dc661ec4604ac1f3d417f33e
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-09-14 12:55:12 -0300

    ready

--- lib/app/modules/pokedex/domain/errors/errors.dart
+++
@@ -1,3 +0,0 @@
-abstract class FailurePokedex implements Exception{
-  
-}
--- lib/app/modules/pokedex/domain/repositories/pokedex_repository.dart
+++ lib/app/modules/pokedex/domain/repositories/pokedex_repository.dart
@@ -1,5 +1,6 @@
 // Aqui eu faço os contratos de implementação, ou seja, eu defino o que eu preciso para implementar a regra de negócio
+import '../model/pokemon_model.dart';
+
 abstract class PokedexRepository {
-  // Future<Either<FailurePokedex, List<Pokemon>>> getAllPokemon();
-  // ai instancio na usecase 
+  Future<List<PokemonModel>> getAllPokemon();
 }
