caeadcb8db99f864eef90bd32fca07e13eddbee7
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-09-15 16:14:11 -0300

    ready

--- lib/app/modules/pokedex/external/pokedex_api.dart.dart
+++ lib/app/modules/pokedex/external/pokedex_api.dart.dart
@@ -5,19 +5,20 @@ import 'package:pokedex_2022/app/modules/pokedex/infra/datasource/pokedex_dataso
 import 'package:http/http.dart' as https;
 
 import '../../../../core/consts/api_pokemon_const.dart';
-//Aqui é onde implementa, poderia ser chamado de PokedexDatasourceImpDart
-class PokedexApi implements PokedexDatasource {
 
+class PokedexApi implements PokedexDatasource {
   @override
   Future<List<PokemonModel>> getAllPokemon() async {
     final response = await https.get(Uri.parse(ApiPokemonConst.allPokemonsURL));
 
     try {
-      var decodedJson = jsonDecode(response.body);
+      var pokemonList = jsonDecode(response.body);
+
+      return pokemonList.map<PokemonModel>((alunoMap) {
+        return PokemonModel.fromMap(alunoMap);
+      }).toList();
     } catch (e) {
-      print('Erro ao fazer a requisição');
+      return throw ('Erro ao carregar lista de pokemons');
     }
   }
-  
-  
 }
